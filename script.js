2100c76fb59ebe511b70625de34e9f8f9768bbbe
Aman Kumar <124803086+Amandf@users.noreply.github.com>
2024-02-21 19:42:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+/*toggle action*/
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx bx-x');
+    navbar.classList.toggle('active')
+};
+
+
+
+/*============================scroll section active link================*/
+let section = document.querySelectorAll('section');
+let navLinks  = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    section.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLink.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY >100 );
+
+
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
+
+
+
+ScrollReveal({ 
+    reset: true, 
+    distance : '80px',
+    duration : 2000,
+    display: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading,', { origin: 'top' });
+ScrollReveal().reveal('.service-container, .portfolio-box, .contact form,', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'left' });
+
+
+ /**===================typed js============================ */
+ const typed = new Typed('.multiple-text', {
+    strings: [ 'Web Developer', 'Student', 'Gamer' ],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+ });
+
+
